ef89c66d7cb96debe7cfd12a1b594c5c2280c3de
hannah holt <hannahholt34@gmail.com>
2022-12-21 16:31:11 -0700

    doctor

--- client/src/components/users/User.js
+++ client/src/components/users/User.js
@@ -3,21 +3,34 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const User = ({}) => {
- const [user, setUser] = useState({ first_name: '', Last_name: '' })
+ const [user, setUser] = useState({ first_name: '', last_name: '' })
+ const [doctors, setDoctors] = useState([])
  const { userId } = useParams()
 
  useEffect( () => {
-   axios.get(`/api/users/${userId}`)
-     .then( res => setUser(res.data) )
-     .catch( err => console.log(err) )
- }, [])
+  axios.get(`/api/users/${userId}`)
+    .then( res => setUser(res.data) )
+    .catch( err => console.log(err) )
+}, [])
 
- const { first_name, last_name } = user
- return (
-    <>
-     <h1>{first_name}</h1>
-    </>
-  )
+useEffect( () => {
+  axios.get(`/api/${userId}/doctorss`)
+    .then( res => setDoctors(res.data) )
+    .catch( err => console.log(err) )
+}, [])
+
+const { first_name, last_name } = user
+return (
+  <>
+   <h1>{first_name} {last_name}</h1>
+   <h1>Appointments</h1>
+   <ListGroup>
+     { courses.map((c) => (
+       <ListGroup.Item>{c.title}</ListGroup.Item>
+     ))}
+   </ListGroup>
+  </>
+)
 }
 
 export default User;
